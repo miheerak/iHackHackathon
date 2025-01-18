c4ff44ac41c88d2a22b766cc9b4de6e7b65359bf
miheerak <ksmiheera@gmail.com>
2025-01-18 14:22:54 +0530

    modified filepath in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, url_for, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
-basedir = os.path.abspath(os.path.dirname(C:\Users\ksmih\iHackfolder\iHackHackathon\app.py))
+basedir = os.path.abspath(os.path.dirname(f'C:\\Users\\ksmih\\iHackfolder\\iHackHackathon\\app.py'))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
