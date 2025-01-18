78b5cfb9736dcef0896efd247a2468a2a5b7e7d7
miheerak <ksmiheera@gmail.com>
2025-01-18 11:54:49 +0530

    integrated flask and sql alchemy, created database.py file

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 #main application, handles backend logic
 
+import os
+from flask import Flask, request, url_for, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+basedir = os.path.abspath(os.path.dirname(C:\Users\ksmih\iHackfolder\iHackHackathon\app.py))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
