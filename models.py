5bebf1ba1af050da4fca0a5a944b7b2d2394b6ea
miheerak <ksmiheera@gmail.com>
2025-01-18 14:33:39 +0530

    modified models.py, added feedback class

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True, nullable=False)
+    username = Column(String(80), unique=True, nullable=False)
     email = Column(String(100), unique=True, nullable=False
     password = Column(String(100), unique=True, nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
@@ -14,3 +14,13 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), unique=True, nullable=False)
+    message=Column(Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Feedback {self.name}>'
