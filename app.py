5aa97fd6a7c92fe26fff8afe5d2c21cc8a92d0bf
miheerak <ksmiheera@gmail.com>
2025-01-19 13:22:37 +0530

    modified index.html and app.py, created registration confirmation url registrationpage.html

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
 if __name__ == "__main__":
 	app.run()
 
-@app.route('registration-form', methods=['POST'])
+@app.route('registrationpage.html', methods=['POST'])
 def register():
     username = request.form['username']
     email = request.form['email']
